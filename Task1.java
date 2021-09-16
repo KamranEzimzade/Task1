1f8b3c85f40b55997a3a98f99621b2805ed81f13
KamranEzimzade <58258360+KamranEzimzade@users.noreply.github.com>
2021-09-16 09:48:22 +0400

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -5,9 +5,16 @@ import java.util.List;
 
 public class Task1 {
 
+    // In the line below a List of numbers with the Long type and "int k" which is the number of cars needs to be covered by the roof is being sent to the function as an argument
+    
     public static long carParkingRoof(List<Long> cars, int k) {
         long distance = Integer.MAX_VALUE;
+        
+        
+        // In the line below with collection.sort() function number of cars(which is 4 cars in this case) is being sorted in ascending order
         Collections.sort(cars);
+        
+        
         for (int i = 0; i <= cars.size() - k; i++) {
 
             distance = Math.min(distance, cars.get(k + i - 1) - cars.get(i));
